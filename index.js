e54253279a5e64fb0a7e60f9e176bb95784ea445
Justin Davis <justin_w_davis@yahoo.com>
2018-10-26 02:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const shout = ( string ) => string.toUpperCase()
 
 const whisper = ( string ) => string.toLowerCase()
 
-const logShout = ( shout ) => console.log( whisper( shout ))
+const logShout = ( string ) => {
+  spy( shout( string ))
+}
